a479bdc31b4988be3ca2a69f0f7d89ce2ba53699
KevinTMO <kevin.ramos3@hotmail.com>
2021-06-15 00:34:50 +0000

    Updated

--- sort.h
+++ sort.h
@@ -11,6 +11,7 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
     const int n;
@@ -18,9 +19,14 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/* Prototypes Functions */
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
 
 #endif /* SORT_H */
