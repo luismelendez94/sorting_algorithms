9aa38d465cd5b0f948d08a5e91404f32c81adf0e
KevinTMO <kevin.ramos3@hotmail.com>
2021-06-15 01:44:01 +0000

    Task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ *bubble_sort - function that sort an array as bubble style
+ *@array: pointer to an array of integers
+ *@size: size of the array
+ *Return: will return nothing (void)
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t iterPos, idx, temp;
+
+	for (iterPos = 0; iterPos < size - 1; iterPos++)
+	{
+		for (idx = 0; idx < size - iterPos - 1; idx++)
+		{
+			if (array[idx] > array[idx + 1])
+			{
+				temp = array[idx];
+				array[idx] = array[idx + 1];
+				array[idx + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
